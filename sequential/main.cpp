a660159637d8c8d74ad6598355e56ed7b8978d6d
Miha Jamsek <miha_jamsek@windowslive.com>
2020-06-06 14:48:31 +0200

    added params

--- sequential/main.cpp
+++ sequential/main.cpp
@@ -1,108 +1,146 @@
 #include <iostream>
 #include <stdlib.h>
 #include <math.h>
+#include <stdio.h>
 
-#define M_SIZE 20
-#define N_SIZE 20
-const double EPSILON = 0.001;
 
-double table[M_SIZE][N_SIZE];
-double tableOld[M_SIZE][N_SIZE];
-
-void print2DArray(double table[M_SIZE][N_SIZE]) {
-	std::cout << "[" << std::endl;
-	for (int i = 0; i < M_SIZE; i++) {
-		std::cout << "[";
-		for (int j = 0; j < N_SIZE; j++) {
-			if (j != 0) {
-				std::cout << ", ";
-			}
-			std::cout << table[i][j];
-		}
-		std::cout << "]" << std::endl;
-	}
-	std::cout << "]" << std::endl;
+double ** initTable(int m, int n) {
+    double ** table = (double **)malloc(m * sizeof(double *));
+    for (int i = 0; i < m; i++) {
+        table[i] = (double *)malloc(n * sizeof(double));
+    }
+    return table;
 }
 
+void print2DArray(double ** table, int m, int n) {
+    printf("[\n");
+    for (int i = 0; i < m; i++) {
+        printf("[");
+        for (int j = 0; j < n; j++) {
+            if (j != 0) {
+                printf(", ");
+            }
+            printf("%6.2f", table[i][j]);
+        }
+        printf("]\n");
+    }
+    printf("]\n");
+}
 
-int main() {
+void initArgs(int argc, const char * argv[], int * table_w, int * table_h, int * tile_w, int * tile_h, double * eps) {
+    // Defaults
+    *table_w = 20;
+    *table_h = 20;
+    *tile_h = 1;
+    *tile_w = 1;
+    *eps = 0.001;
 
-	/*
-	  Initializing table borders
-	*/
-	for (int i = 1; i < M_SIZE - 1; i++) {
-		table[i][0] = 100.0;
-	}
-	for (int i = 1; i < M_SIZE - 1; i++) {
-		table[i][N_SIZE - 1] = 100.0;
-	}
-	for (int j = 0; j < N_SIZE; j++) {
-		table[M_SIZE - 1][j] = 100.0;
-	}
-	for (int j = 0; j < N_SIZE; j++) {
-		table[0][j] = 0.0;
-	}
+    if (argc < 3) {
+        return;
+    } else if (argc >= 3 && argc < 5) {
+        *table_w = atoi(argv[1]);
+        *table_h = atoi(argv[2]);
+    } else if (argc == 5) {
+        *table_w = atoi(argv[1]);
+        *table_h = atoi(argv[2]);
+        *tile_w = atoi(argv[3]);
+        *tile_h = atoi(argv[4]);
+    } else if (argc >= 6) {
+        *table_w = atoi(argv[1]);
+        *table_h = atoi(argv[2]);
+        *tile_w = atoi(argv[3]);
+        *tile_h = atoi(argv[4]);
+        *eps = atof(argv[5]);
+    }
+}
 
-	/*
-	  Setting average value in middle tiles
-	*/
-	double average = 0.0;
-	for (int i = 1; i < M_SIZE - 1; i++) {
-		average = average + table[i][0] + table[i][N_SIZE - 1];
-	}
-	for (int j = 0; j < N_SIZE; j++) {
-		average = average + table[M_SIZE - 1][j] + table[0][j];
-	}
-	average = average / (double)(2 * M_SIZE + 2 * N_SIZE - 4);
+int main(int argc, const char * argv[]) {
+    int W, H, w, h;
+    double EPSILON;
+    initArgs(argc, argv, &W, &H, &w, &h, &EPSILON);
+    int M = H / h;
+    int N = W / w;
 
-	for (int i = 1; i < M_SIZE - 1; i++) {
-		for (int j = 1; j < N_SIZE - 1; j++) {
-			table[i][j] = average;
-		}
-	}
+    double ** table = initTable(M, N);
+    double ** tableOld = initTable(M, N);
+    /*
+      Initializing table borders
+    */
+    for (int i = 1; i < M - 1; i++) {
+        table[i][0] = 100.0;
+    }
+    for (int i = 1; i < M - 1; i++) {
+        table[i][N - 1] = 100.0;
+    }
+    for (int j = 0; j < N; j++) {
+        table[M - 1][j] = 100.0;
+    }
+    for (int j = 0; j < N; j++) {
+        table[0][j] = 0.0;
+    }
 
-	// print2DArray(table);
+    /*
+      Setting average value in middle tiles
+    */
+    double average = 0.0;
+    for (int i = 1; i < M - 1; i++) {
+        average = average + table[i][0] + table[i][N - 1];
+    }
+    for (int j = 0; j < N; j++) {
+        average = average + table[M - 1][j] + table[0][j];
+    }
+    average = average / (double)(2 * M + 2 * N - 4);
 
-	/*
-	  Algorithm start
-	*/
-	int iterations = 0;
-	double diff = EPSILON;
+    for (int i = 1; i < M - 1; i++) {
+        for (int j = 1; j < N - 1; j++) {
+            table[i][j] = average;
+        }
+    }
 
-	while (EPSILON <= diff) {
+    // print2DArray(table);
 
-		// Copy table to old table
-		for (int i = 0; i < M_SIZE; i++) {
-			for (int j = 0; j < N_SIZE; j++) {
-				tableOld[i][j] = table[i][j];
-			}
-		}
+    /*
+      Algorithm start
+    */
+    int iterations = 0;
+    double diff = EPSILON;
 
-		// Calculate new values
-		for (int i = 1; i < M_SIZE - 1; i++) {
-			for (int j = 1; j < N_SIZE - 1; j++) {
-				table[i][j] = (tableOld[i - 1][j] + tableOld[i + 1][j] + tableOld[i][j - 1] + tableOld[i][j + 1]) / 4.0;
-			}
-		}
+    while (EPSILON <= diff) {
 
-		diff = 0.0;
-		for (int i = 1; i < M_SIZE - 1; i++) {
-			for (int j = 1; j < N_SIZE - 1; j++) {
-				double localDiff = fabs(table[i][j] - tableOld[i][j]);
-				if (diff < localDiff) {
-					// std::cout << "Changing diff from " << diff << ", to " << localDiff << std::endl;
-					diff = localDiff;
-				}
-			}
-		}
+        // Copy table to old table
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                tableOld[i][j] = table[i][j];
+            }
+        }
 
-		iterations++;
-		// std::cout << "Diff: " << diff << ", Eps: " << EPSILON << std::endl;
-	}
+        // Calculate new values
+        for (int i = 1; i < M - 1; i++) {
+            for (int j = 1; j < N - 1; j++) {
+                table[i][j] = (tableOld[i - 1][j] + tableOld[i + 1][j] + tableOld[i][j - 1] + tableOld[i][j + 1]) / 4.0;
+            }
+        }
 
-	std::cout << "Number of iterations: " << iterations << std::endl;
+        diff = 0.0;
+        // 
+        for (int i = 1; i < M - 1; i++) {
+            for (int j = 1; j < N - 1; j++) {
 
-	print2DArray(table);
+                double localDiff = fabs(table[i][j] - tableOld[i][j]);
+                if (diff < localDiff) {
+                    // std::cout << "Changing diff from " << diff << ", to " << localDiff << std::endl;
+                    diff = localDiff;
+                }
+            }
+        }
 
-	return 0;
+        iterations++;
+        // std::cout << "Diff: " << diff << ", Eps: " << EPSILON << std::endl;
+    }
+
+    std::cout << "Number of iterations: " << iterations << std::endl;
+
+    print2DArray(table, M, N);
+
+    return 0;
 }
